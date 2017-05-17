715c1e56b9b597c2de560eeac9f1154461dfa127
Horv <tamasurfi@gmail.com>
2017-05-17 22:57:44 +0200

    + Functions which install libs + Find and install libs from R script

---
+++ example.R
@@ -0,0 +1,9 @@
+library('RUnit')
+print('Hello World!')
+
+require('caret') 
+library(AICcmodavg)
+require(car)
+
+
+print('From example')
--- installr.R
+++ installr.R
@@ -1,10 +1,27 @@
 library('yaml')
 
+InstallLibs <- function(libs, load = TRUE) {
+  sapply(libs, 
+         function(lib) {
+           if (!(lib %in% installed.packages()[, 1]))
+             install.packages(lib)
+           if (load)
+             library(lib, character.only = TRUE)
+         }
+  )
+}
 
-libs <- yaml.load_file('deps.yaml')
-sapply(libs$deps, 
-       function(lib) {
-         if (!(lib %in% installed.packages()[, 1]))
-           install.packages(lib)
-       }
-)
+InstallLibsScript <- function(files, load = TRUE) {
+  contents <- sapply(files, function(file) readLines(file, file.size(file)))
+  libs <- c(gsub("library\\((.*)\\)", "\\1", contents)[grep("library", contents), ],
+            gsub("require\\((.*)\\)", "\\1", contents)[grep("require", contents), ])
+  libs <- gsub("[^[:alnum:]]", "", libs)
+  InstallLibs(libs, load)
+}
+
+
+InstallLibsYAML <- function(file = 'deps.yaml') {
+  libs <- yaml.load_file(file)
+  InstallLibs(libs$deps, libs$load)
+}
+
